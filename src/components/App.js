6aaecf20a11e61fb0daae125132419858bc210b8
Andrew Leigh <silverstone_12@hotmail.co.uk>
2017-07-29 13:30:58 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+// Default create-react-app
+import React, { Component } from "react";
+import { Grid, Navbar } from 'react-bootstrap';
+
+// Images
+import logo from "../images/logo.svg";
+
+// Styles
+import "../css/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="center">
+          blah
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+// Default create-react-app
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap-theme.css";
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
